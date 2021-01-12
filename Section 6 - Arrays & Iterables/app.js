7f600a1de6cb12d1aff72d6159e3252144499f3b
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-01-12 21:27:25 +0300

    sort()ing and reverse()ing

--- Section 6 - Arrays & Iterables/app.js
+++ Section 6 - Arrays & Iterables/app.js
@@ -91,9 +91,9 @@
 
 // console.log(taxAdjustedPrace);
 
-// 195
+// 195 + 196
 
-const prices = [10.99, 5.99, 3.99, 5.59];
+const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
 const taxAdjustedPrace = prices.map((price, idx, prices) => {
@@ -101,4 +101,16 @@ const taxAdjustedPrace = prices.map((price, idx, prices) => {
   return priceObj;
 });
 
-console.log(taxAdjustedPrace);
+// console.log(taxAdjustedPrace);
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1; // -1
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1; // 1
+  }
+});
+// console.log(sortedPrices.reverse());
+console.log(sortedPrices);
