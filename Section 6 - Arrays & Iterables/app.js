6ad14d3124e4d7222bd6d7de1a720a4a5ec9fc55
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-01-12 20:48:50 +0300

    The forEach() method

--- Section 6 - Arrays & Iterables/app.js
+++ Section 6 - Arrays & Iterables/app.js
@@ -43,31 +43,48 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// const storedResults = testResults.slice(2);
-const storedResults = testResults.concat([3.99, 2]);
+// 
 
-testResults.push(5.91);
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // const storedResults = testResults.slice(2);
+// const storedResults = testResults.concat([3.99, 2]);
 
-console.log(storedResults, testResults);
-console.log(testResults.indexOf(1.5));
+// testResults.push(5.91);
 
-console.log(testResults.includes(10.99));
-console.log(testResults.indexOf(10.99) !== -1);
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5));
 
-const personData = [{ name: 'Max' }, { name: 'Manuel' }];
-console.log(personData.indexOf({ name: 'Manuel' }));
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) !== -1);
 
-const manuel = personData.find((person, idx, persons) => {
-  return person.name === 'Manuel';
+// const personData = [{ name: 'Max' }, { name: 'Manuel' }];
+// console.log(personData.indexOf({ name: 'Manuel' }));
+
+// const manuel = personData.find((person, idx, persons) => {
+//   return person.name === 'Manuel';
+// });
+
+// manuel.name = 'Anna';
+
+// console.log(manuel, personData);
+
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//   return person.name === 'Max';
+// });
+
+// console.log(maxIndex);
+
+const prices = [10.99, 5.99, 3.99, 5.59];
+const tax = 0.19;
+const taxAdjustedPrace = [];
+
+// for (const price of prices) {
+//   taxAdjustedPrace.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjustedPrace: price * (1 + tax) };
+  taxAdjustedPrace.push(priceObj);
 });
 
-manuel.name = 'Anna';
-
-console.log(manuel, personData);
-
-const maxIndex = personData.findIndex((person, idx, persons) => {
-  return person.name === 'Max';
-});
-
-console.log(maxIndex);
+console.log(taxAdjustedPrace);
