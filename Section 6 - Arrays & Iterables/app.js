12f8d2fdf92ddc1527f62f6dce1819bac841e852
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-01-18 02:59:29 +0300

    Array Section

--- Section 6 - Arrays & Iterables/app.js
+++ Section 6 - Arrays & Iterables/app.js
@@ -43,8 +43,6 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-// 
-
 // const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 // // const storedResults = testResults.slice(2);
 // const storedResults = testResults.concat([3.99, 2]);
@@ -74,71 +72,89 @@
 
 // console.log(maxIndex);
 
-// 194
-
-// const prices = [10.99, 5.99, 3.99, 5.59];
+// const prices = [10.99, 5.99, 3.99, 6.59];
 // const tax = 0.19;
-// const taxAdjustedPrace = [];
+// const taxAdjustedPrices = [];
 
 // // for (const price of prices) {
-// //   taxAdjustedPrace.push(price * (1 + tax));
+// //   taxAdjustedPrices.push(price * (1 + tax));
 // // }
 
 // prices.forEach((price, idx, prices) => {
-//   const priceObj = { index: idx, taxAdjustedPrace: price * (1 + tax) };
-//   taxAdjustedPrace.push(priceObj)
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
 // });
 
-// console.log(taxAdjustedPrace);
+// console.log(taxAdjustedPrices);
 
-// 195 + 196 + 197
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
-
-const taxAdjustedPrace = prices.map((price, idx, prices) => {
-  const priceObj = { index: idx, taxAdjustedPrace: price * (1 + tax) };
-  return priceObj;
-});
-
-// console.log(taxAdjustedPrace);
-
-const sortedPrices = prices.sort((a, b) => {
-  if (a > b) {
-    return 1; // -1
-  } else if (a === b) {
-    return 0;
-  } else {
-    return -1; // 1
-  }
-});
-// console.log(sortedPrices.reverse());
-console.log(sortedPrices);
-
-// const filteredArray = prices.filter((price, index, prices) => {
-//   return price > 6;
+// const taxAdjustedPrices = prices.map((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   return priceObj;
 // });
 
-const filteredArray = prices.filter(price => price > 6);
+// // console.log(prices, taxAdjustedPrices);
 
-console.log(filteredArray);
-
-// 199
-
-// method 1
-
-// let sum = 0;
-
-// prices.forEach((prices) => {
-//   sum += prices;
+// const sortedPrices = prices.sort((a, b) => {
+//   if (a > b) {
+//     return -1;
+//   } else if (a === b) {
+//     return 0;
+//   } else {
+//     return 1;
+//   }
 // });
+// // console.log(sortedPrices.reverse());
+// console.log(sortedPrices);
+
+// const filteredArray = prices.filter(p => p > 6);
+
+// console.log(filteredArray);
+
+// // let sum = 0;
+
+// // prices.forEach((price) => {
+// //   sum += price
+// // });
+
+// // console.log(sum);
+
+// const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
 
 // console.log(sum);
 
-// method 2
+// const data = 'new york;10.99;2000';
 
-const sum = prices.reduce((prevValue, CurValue) => {
-  return prevValue + CurValue;
-});
+// const transformedData = data.split(';');
+// transformedData[1] = +transformedData[1];
+// console.log(transformedData);
 
-console.log(sum);
+// const nameFragments = ['Max', 'Schwarz'];
+// const name = nameFragments.join(' ');
+// console.log(name);
+
+// const copiedNameFragments = [...nameFragments];
+// nameFragments.push('Mr');
+// console.log(nameFragments, copiedNameFragments);
+
+// console.log(Math.min(...prices));
+
+// const persons = [{ name: 'Max', age: 30 }, { name: 'Manuel', age: 31 }];
+// const copiedPersons = persons.map(person => ({
+//   name: person.name,
+//   age: person.age
+// }));
+
+// persons.push({ name: 'Anna', age: 29 });
+// persons[0].age = 31;
+
+// console.log(persons, copiedPersons);
+
+const nameData = ['Max', 'Schwarz', 'Mr', 30];
+// const firstName = nameData[0];
+// const lastName = nameData[1];
+
+const [ firstName, lastName, ...otherInformation ] = nameData;
+console.log(firstName, lastName, otherInformation);
