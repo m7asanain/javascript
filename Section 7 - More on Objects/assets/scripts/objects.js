6b52bb6b102208bee3add85522c0824688062d1f
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-01-18 03:01:49 +0300

    Object Section

---
+++ Section 7 - More on Objects/assets/scripts/objects.js
@@ -0,0 +1,74 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const randerMovie = (filter = '') => {
+  // display the result on the screen
+  const movieList = document.getElementById('movie-list');
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+  const filterdMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+  filterdMovies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    // const { title: movieTitle } = info;
+    let { getFormatedTitle } = movie;
+    getFormatedTitle = getFormatedTitle.bind(movie);
+    let text = getFormatedTitle() + ' - ';
+    for (const key in info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${info[key]}`;
+      }
+    }
+    movieList.append(movieEl);
+    movieEl.textContent = text;
+    
+  });
+};
+
+const addMovieHandler = () => {
+//  // getting input here
+  const title = document.getElementById('title').value;
+  const exteraName = document.getElementById('extra-name').value;
+  const exteraValue = document.getElementById('extra-value').value;
+//
+  if (
+    title.trim() === '' ||
+    exteraName.trim() === '' ||
+    exteraValue.trim() === ''
+  ) {
+    return;
+  }
+//
+//  // set an object for the array
+  const newMovie = {
+    info: {
+      title: title,
+      [exteraName]: exteraValue
+    },
+    id: Math.random().toString(),
+    // getFormatedTitle: function() {
+    getFormatedTitle() {
+      return this.info.title.toUpperCase();
+    }
+  }
+
+  movies.push(newMovie);
+  randerMovie();
+  console.log(movies);
+
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  randerMovie(filterTerm);
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
