e37fd3dc270f56e9643d2909f3330831777b75b4
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-03-30 21:04:29 +0300

    new start oop

--- Section 8 - OOP/assets/scripts/app.js
+++ Section 8 - OOP/assets/scripts/app.js
@@ -1,203 +1,40 @@
-class Product {
-    // title = 'DEFAULT';
-    // imageUrl;
-    // dsescription;
-    // price;
-
-    constructor(title, image, dsec, price) {
-        this.title = title;
-        this.imageUrl = image;
-        this.dsescription = dsec;
-        this.price = price;
-    };
-}
-
-class ShoppingCart {
-    items = [];
-
-    set cartItems(value) {
-        this.items = value;
-        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
-
-    };
-
-    get totalAmount() {
-        const sum = this.items.reduce(
-            (prevValue, curValue) => prevValue + curValue.price, 
-            0
-        );
-        return sum;
-    };
-
-    addProduct(product) {
-        // this.items.push(product);
-        const updatedItems = [...this.items];
-        updatedItems.push(product);
-        this.cartItems = updatedItems;
-    };
-
+const productList = {
+    products: [
+        {
+            title: 'A Pillow',
+            imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ghi-best-pillows-1573668641.png?crop=0.946xw:0.727xh;0.0288xw,0.163xh&resize=1200:*',
+            dsescription: 'A soft pillow',
+            price: 19.99
+        },
+        {
+            title: 'A Carpit',
+            imageUrl: 'https://i.pinimg.com/236x/a1/43/be/a143bebe70cbb2b48826e04c833e39fc--turkish-carpets-magic-carpet.jpg',
+            dsescription: 'A Carpit which you might like - or not!',
+            price: 89.99
+        }
+    ],
     render() {
-        const cartEl = document.createElement('section');
-        cartEl.innerHTML = `
-            <h2>Total: \$${0}</h2>
-            <button>Order Now!</button>
-        `;
-        cartEl.className = 'cart';
-        this.totalOutput = cartEl.querySelector('h2');
-        return cartEl;
-    };
-}
-
-// console.log(new Product());
-
-class ProductItems {
-    constructor(product) {
-        this.product = product;
-    };
-
-    addToCart() {
-        App.addProductToCart(this.product);
-    };
-
-    render() {
-        const prodEl = document.createElement('li');
-        prodEl.className = 'product-item';
-        prodEl.innerHTML = `
-            <div class="product-item__content">
-                <img src="${this.product.imageUrl}" alt="${this.product.title}">
-                <div>
-                    <h2>${this.product.title}</h2>
-                    <h3>\$${this.product.price}</h3>
-                    <p>${this.product.dsescription}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-        `;
-        const addCartButton = prodEl.querySelector('button');
-        addCartButton.addEventListener('click', this.addToCart.bind(this));
-        return prodEl;
-    };
-}
-
-class ProductList {
-    products = [
-        new Product(
-            'A Pillow',
-            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ghi-best-pillows-1573668641.png?crop=0.946xw:0.727xh;0.0288xw,0.163xh&resize=1200:*',
-            'A soft pillow', 
-            19.99
-        ),
-        new Product(
-            'A Carpit',
-            'https://i.pinimg.com/236x/a1/43/be/a143bebe70cbb2b48826e04c833e39fc--turkish-carpets-magic-carpet.jpg',
-            'A Carpit which you might like - or not!',
-            89.99
-        ),
-    ];
-    render() {
-        // #3
-        // const renderHook = document.getElementById('app');
+        const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const productList = new ProductItems(prod);
-            const prodEl = productList.render();
-            // const prodEl = document.createElement('li');
-            // prodEl.className = 'product-item';
-            // prodEl.innerHTML = `
-            //     <div class="product-item__content">
-            //         <img src="${prod.imageUrl}" alt="${prod.title}">
-            //         <div>
-            //             <h2>${prod.title}</h2>
-            //             <h3>\$${prod.price}</h3>
-            //             <p>${prod.dsescription}</p>
-            //             <button>Add to Cart</button>
-            //         </div>
-            //     </div>
-            // `;
+            const prodEl = document.createElement('li');
+            prodEl.className = 'product-item';
+            prodEl.innerHTML = `
+                <div class="product-item__content">
+                    <img src="${prod.imageUrl}" alt="${prod.title}">
+                    <div>
+                        <h2>${prod.title}</h2>
+                        <h3>\$${prod.price}</h3>
+                        <p>${prod.dsescription}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            `;
             prodList.append(prodEl);
         }
-        // renderHook.append(prodList);
-        return prodList;
-    };
-}
-
-// !!!!!!!!! siplete this code to the class !!!!!!!!!!!!!
-
-// const productList = {
-//     products: [
-//         new Product(
-//             'A Pillow',
-//             'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ghi-best-pillows-1573668641.png?crop=0.946xw:0.727xh;0.0288xw,0.163xh&resize=1200:*',
-//             'A soft pillow', 
-//             19.99
-//         ),
-//         new Product(
-//             'A Carpit',
-//             'https://i.pinimg.com/236x/a1/43/be/a143bebe70cbb2b48826e04c833e39fc--turkish-carpets-magic-carpet.jpg',
-//             'A Carpit which you might like - or not!',
-//             89.99
-//         ),
-//     ],
-//     render() {
-//         const renderHook = document.getElementById('app');
-//         const prodList = document.createElement('ul');
-//         prodList.className = 'product-list';
-//         for (const prod of this.products) {
-//             const prodEl = document.createElement('li');
-//             prodEl.className = 'product-item';
-//             prodEl.innerHTML = `
-//                 <div class="product-item__content">
-//                     <img src="${prod.imageUrl}" alt="${prod.title}">
-//                     <div>
-//                         <h2>${prod.title}</h2>
-//                         <h3>\$${prod.price}</h3>
-//                         <p>${prod.dsescription}</p>
-//                         <button>Add to Cart</button>
-//                     </div>
-//                 </div>
-//             `;
-//             prodList.append(prodEl);
-//         }
-//         renderHook.append(prodList);
-//     }
-// };
-
-// productList.render();
-
-class Shop {
-    render() {
-        // #4
-        const renderHook = document.getElementById('app');
-
-        // #2
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
-        const productList = new ProductList();
-        const prodListEl = productList.render();
-
-        
-        renderHook.append(cartEl);
-        renderHook.append(prodListEl);
+        renderHook.append(prodList);
     }
-}
+};
 
-// #1
-// const productList = new ProductList();
-// productList.render();
-
-class App {
-    static cart;
-    
-    static init() {
-        const shop = new Shop();
-        shop.render();
-        this.cart = shop.cart;
-    }
-
-    static addProductToCart(product) {
-        this.cart.addProduct(product);
-    }
-}
-
-App.init();
+productList.render();
