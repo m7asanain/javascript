0bc38c74fa432059b25c9102f94bb81201888057
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-03-31 01:51:49 +0300

    Working with Constructor Methods

--- Section 8 - OOP/assets/scripts/app.js
+++ Section 8 - OOP/assets/scripts/app.js
@@ -1,17 +1,25 @@
+class product {
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.dsescription = desc;
+        this.price = price;
+    }
+}
+
 const productList = {
     products: [
-        {
-            title: 'A Pillow',
-            imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ghi-best-pillows-1573668641.png?crop=0.946xw:0.727xh;0.0288xw,0.163xh&resize=1200:*',
-            dsescription: 'A soft pillow',
-            price: 19.99
-        },
-        {
-            title: 'A Carpit',
-            imageUrl: 'https://i.pinimg.com/236x/a1/43/be/a143bebe70cbb2b48826e04c833e39fc--turkish-carpets-magic-carpet.jpg',
-            dsescription: 'A Carpit which you might like - or not!',
-            price: 89.99
-        }
+        new product('A Pillow',
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ghi-best-pillows-1573668641.png?crop=0.946xw:0.727xh;0.0288xw,0.163xh&resize=1200:*',
+            'A soft pillow',
+            19.99
+        ),
+
+        new product('A Carpit',
+            'https://i.pinimg.com/236x/a1/43/be/a143bebe70cbb2b48826e04c833e39fc--turkish-carpets-magic-carpet.jpg',
+            'A Carpit which you might like - or not!',
+            89.99
+        ),
     ],
     render() {
         const renderHook = document.getElementById('app');
