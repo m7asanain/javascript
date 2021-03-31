c527f48c53b9244d208bdc819535ea5703835418
m7asanain <63984328+m7asanain@users.noreply.github.com>
2021-04-01 01:21:42 +0300

    Using & "Connecting" Multiple Classes

--- Section 8 - OOP/assets/scripts/app.js
+++ Section 8 - OOP/assets/scripts/app.js
@@ -7,42 +7,58 @@ class product {
     }
 }
 
-const productList = {
-    products: [
-        new product('A Pillow',
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+            <div class="product-item__content">
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div>
+                    <h2>${this.product.title}</h2>
+                    <h3>\$${this.product.price}</h3>
+                    <p>${this.product.dsescription}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
+        new product(
+            'A Pillow',
             'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ghi-best-pillows-1573668641.png?crop=0.946xw:0.727xh;0.0288xw,0.163xh&resize=1200:*',
             'A soft pillow',
             19.99
         ),
-
-        new product('A Carpit',
+        new product(
+            'A Carpit',
             'https://i.pinimg.com/236x/a1/43/be/a143bebe70cbb2b48826e04c833e39fc--turkish-carpets-magic-carpet.jpg',
             'A Carpit which you might like - or not!',
             89.99
         ),
-    ],
+    ];
+
+    constructor() {}
+
     render() {
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-                <div class="product-item__content">
-                    <img src="${prod.imageUrl}" alt="${prod.title}">
-                    <div>
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.dsescription}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
-};
+}
 
+const productList = new ProductList;
 productList.render();
