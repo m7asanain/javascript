98cf2ba55c96c4a0f916941fb8290851761ee39e
Mustafa Alkamali <m7aanain@gmail.com>
2021-06-03 00:30:15 +0300

    Switch Project

--- Section 9 - Practice OOP & Classes/assets/scripts/app.js
+++ Section 9 - Practice OOP & Classes/assets/scripts/app.js
@@ -1,89 +1,51 @@
-class DOMHelper {
-  static clearEventListeners(element) {
-    const clonedElement = element.cloneNode(true);
-    element.replaceWith(clonedElement);
-    return clonedElement;
-  }
-
-  static moveElement(elementId, newDestinationSelector) {
-    const element = document.getElementById(elementId);
-    const destinationElement = document.querySelector(newDestinationSelector);
-    destinationElement.append(element);
-  }
-}
-
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id, updateProjectListsFunction, type) {
-    this.id = id;
-    this.updateProjectListsHandler = updateProjectListsFunction;
-    this.connectMoreInfoButton();
-    this.connectSwitchButton(type);
-  }
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
 
-  connectMoreInfoButton() {}
+    connectMoreInfoButton() {
 
-  connectSwitchButton(type) {
-    const projectItemElement = document.getElementById(this.id);
-    let switchBtn = projectItemElement.querySelector('button:last-of-type');
-    switchBtn = DOMHelper.clearEventListeners(switchBtn);
-    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
-    switchBtn.addEventListener(
-      'click',
-      this.updateProjectListsHandler.bind(null, this.id)
-    );
-  }
+    }
 
-  update(updateProjectListsFn, type) {
-    this.updateProjectListsHandler = updateProjectListsFn;
-    this.connectSwitchButton(type);
-  }
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = projectItemElement.querySelector('button:last-of-type');
+        switchBtn.addEventListener('click', );
+    }
 }
 
 class ProjectList {
-  projects = [];
+    products = [];
 
-  constructor(type) {
-    this.type = type;
-    const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    for (const prjItem of prjItems) {
-      this.projects.push(
-        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
-      );
+    constructor(type) {
+        const projItems = document.querySelectorAll(`#${type}-projects li`);
+        for (const projItem of projItems) {
+            this.products.push(new ProjectItem(projItem.id))
+        }
+        console.log(this.products);
     }
-    console.log(this.projects);
-  }
 
-  setSwitchHandlerFunction(switchHandlerFunction) {
-    this.switchHandler = switchHandlerFunction;
-  }
+    addProject() {
 
-  addProject(project) {
-    this.projects.push(project);
-    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
-    project.update(this.switchProject.bind(this), this.type);
-  }
+    }
 
-  switchProject(projectId) {
-    // const projectIndex = this.projects.findIndex(p => p.id === projectId);
-    // this.projects.splice(projectIndex, 1);
-    this.switchHandler(this.projects.find(p => p.id === projectId));
-    this.projects = this.projects.filter(p => p.id !== projectId);
-  }
+    switchProject(productId) {
+        // const productsIndex = this.products.findIndex(p => p.id === productId);
+        // this.products.slice(productsIndex, 1);
+        this.addProject();
+        this.products = this.products.filter(p => p.id !== productId);
+    }
 }
 
 class App {
-  static init() {
-    const activeProjectsList = new ProjectList('active');
-    const finishedProjectsList = new ProjectList('finished');
-    activeProjectsList.setSwitchHandlerFunction(
-      finishedProjectsList.addProject.bind(finishedProjectsList)
-    );
-    finishedProjectsList.setSwitchHandlerFunction(
-      activeProjectsList.addProject.bind(activeProjectsList)
-    );
-  }
+    static init() {
+        const activeProjectsList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+    }
 }
 
 App.init();
