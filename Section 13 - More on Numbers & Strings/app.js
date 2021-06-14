0d4b59b19a8334f36df9ee01df46394d663fb0a1
Mustafa Alkamali <m7aanain@gmail.com>
2021-06-14 23:05:28 +0300

    Tagged Templates

--- Section 13 - More on Numbers & Strings/app.js
+++ Section 13 - More on Numbers & Strings/app.js
@@ -1,6 +1,21 @@
-function randomInBetween(min, max) {
-    // return Math.floor(Math.random() * (max - min));
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function productDescription(strings, productName, productPrice) {
+    console.log(strings);
+    console.log(productName);
+    console.log(productPrice);
+
+    let priceCategory = 'cheap';
+
+    if (productPrice > 20) {
+        priceCategory = 'fairly priced';
+    }
+
+    // return 'This is a product!';
+    // return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+    return {name: prodName, price: prodPrice};
 }
 
-console.log(randomInBetween(1, 10));
+const prodName = 'JavaScript Course';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
