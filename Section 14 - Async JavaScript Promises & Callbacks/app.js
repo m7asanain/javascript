7d7405bd3f7c11524173745f370da7a0a4dd9e9d
Mustafa Alkamali <m7aanain@gmail.com>
2021-06-17 00:56:44 +0300

    Sync + Async Code - The Execution Order

--- Section 14 - Async JavaScript Promises & Callbacks/app.js
+++ Section 14 - Async JavaScript Promises & Callbacks/app.js
@@ -1,19 +1,25 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(postData => {
+    console.log('The location is: ', postData);
+  }, error => {
+    console.log('couldn\'t get the location.', error);
+  });
+  console.log('Getting location...');
 }
 
 button.addEventListener('click', trackUserHandler);
 
 let result = 0;
 
-for (let i = 0; i < 100000000; i++) {
-  result += i;
-}
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
 
-console.log(result);
+// console.log(result);
 
 
 // const greet = () => {
