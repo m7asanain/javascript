1883ad23a756a6cf119339d130bed7d6a40281c7
Mustafa Alkamali <m7aanain@gmail.com>
2021-06-20 19:27:42 +0300

    JSON Data & Parsing Data

--- Section 15 - Working with Http Requests/assets/scripts/app.js
+++ Section 15 - Working with Http Requests/assets/scripts/app.js
@@ -1,5 +1,29 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json';   // *same, more convenience
+
+xhr.onload = function() {   // addEventListener is not really supported
+    // console.log(xhr.response);  // return all data
+
+    // const listOfPosts = xhr.response;
+    // listOfPosts.push({});   //  not work because it respond to return json no js
+
+    // const listOfPosts = JSON.parse(xhr.response);   // *same
+
+    const listOfPosts = xhr.response;  
+    console.log(listOfPosts);
+
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+    }
+}
 
 xhr.send();
