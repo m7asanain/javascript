98bb4f5262686963bfcace6c51204e443ef27049
Mustafa Alkamali <m7aanain@gmail.com>
2021-06-20 23:14:03 +0300

    Handling Errors

--- Section 15 - Working with Http Requests/assets/scripts/app.js
+++ Section 15 - Working with Http Requests/assets/scripts/app.js
@@ -12,26 +12,38 @@ function sendHttpRequest(method, url, data) {
         xhr.responseType = 'json';
 
         xhr.onload = function() {
-            resolve(xhr.response);
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject(new Error('Something went wrong!')); // if there was an error (link is wrong)
+            }
         }
+        xhr.onerror = function() {
+            reject(new Error('Faild to send request!')); // it will happed if we fail with sending request (connection problem)
+        }
+
         xhr.send(JSON.stringify(data));
     });
     return promise;
 }
 
 async function fetchPosts() {
-    const responseData = await sendHttpRequest(
-        'GET',
-        'https://jsonplaceholder.typicode.com/posts'
-    );
-
-    const listOfPosts = responseData; 
-    for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        postEl.querySelector('li').id = post.id;
-        listElement.append(postEl);
+    try {
+        const responseData = await sendHttpRequest(
+            'GET',
+            'https://jsonplaceholder.typicode.com/posts'
+        );
+    
+        const listOfPosts = responseData; 
+        for (const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            postEl.querySelector('li').id = post.id;
+            listElement.append(postEl);
+        }
+    } catch (error) {
+        alert(error.message);
     }
 }
 
