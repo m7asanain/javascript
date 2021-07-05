59223411b6b22e0db2a70f7e6e5d40b833493225
Mustafa Alkamali <m7asanain@gmail.com>
2021-07-06 01:37:43 +0300

    Getting Started with Cookies

--- Section 19 - Utilizing Browser Storage/app.js
+++ Section 19 - Utilizing Browser Storage/app.js
@@ -1,30 +1,11 @@
 const storeBtn = document.getElementById('store-btn');
 const retrieveBtn = document.getElementById('retrieve-btn');
 
-const userId = 'u123';
-const user = {
-  name: 'Mustafa',
-  age: 20,
-  hobbies: ['Coding', 'Reading', 'Science']
-}
-
 storeBtn.addEventListener('click', () => {
-  // localStorage.setItem('uid', userId);
-  sessionStorage.setItem('uid', userId);
-  localStorage.setItem('user', JSON.stringify(user)); // make it string with json format
+  const userId = 'u123';
+  document.cookie = `uid=${userId}`;
 });
 
 retrieveBtn.addEventListener('click', () => {
-  // const extractedId = localStorage.getItem('uid');
-  const extractedId = sessionStorage.getItem('uid');
-
-  const extractedUser = JSON.parse(localStorage.getItem('user')); // parse it so we could read the data
-
-  console.table(extractedUser);
-
-  if (extractedId) {
-    console.log(`Got the id ${extractedId}`);
-  } else {
-    console.log('Couldn\'t get the id');
-  }
+  console.log(document.cookie);
 });
