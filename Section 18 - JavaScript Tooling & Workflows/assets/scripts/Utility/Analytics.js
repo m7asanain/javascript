7ef9cc4d63a909b424f9ea17daf9d50f73413ebf
Mustafa Alkamali <m7asanain@gmail.com>
2021-07-19 16:57:24 +0300

    New Start

--- Section 18 - JavaScript Tooling & Workflows/assets/scripts/Utility/Analytics.js
+++ Section 18 - JavaScript Tooling & Workflows/assets/scripts/Utility/Analytics.js
@@ -1,11 +1,7 @@
 
-const timerTime = 2000;
-
 const intervalId = setInterval(() => {
-  // eslint-disable-next-line no-console
-  console.log('Sending analytics data...');
-},
-timerTime);
+  console.log('Sending analytics data...')
+}, 2000);
 
 document.getElementById('stop-analytics-btn').addEventListener('click', () => {
   clearInterval(intervalId);
