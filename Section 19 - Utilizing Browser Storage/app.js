e4f74ab81a4e7ff2b70b367c03c6a97c0d2fada0
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-09 14:23:36 +0300

    Start a new section

--- Section 19 - Utilizing Browser Storage/app.js
+++ Section 19 - Utilizing Browser Storage/app.js
@@ -1,23 +1,34 @@
 const storeBtn = document.getElementById('store-btn');
 const retrieveBtn = document.getElementById('retrieve-btn');
 
+const dbRequest = indexedDB.open('DummyStorage', 1);
+
+dbRequest.upgradeneeded = function (event) {  // onsuccess return: Uncaught DOMException 
+  const db = event.target.result;
+
+  const objStore = db.createObjectStore('products', {keyPath: 'id'});
+
+  objStore.transaction.oncomplete = function(event) {
+    const productsStore = db
+      .transaction('products', 'readwrite')
+      .objStore('products');
+    productsStore.add({
+      id: 'p1', 
+      title: 'A First Product', 
+      price: 12.99, 
+      tags: ['Expensive', 'Luxury']
+    });
+  }
+};
+
+dbRequest.onerror = function (event) {
+  console.log('ERROR!');
+};
+
 storeBtn.addEventListener('click', () => {
-  const userId = 'u123';
-  const user = {name: 'Mustafa', age: 20};
-  document.cookie = `uid=${userId}; max-age=2`;
-  document.cookie = `user=${JSON.stringify(user)}`;
+
 });
 
 retrieveBtn.addEventListener('click', () => {
-  // console.log(document.cookie.split(';'));
-  console.log(document.cookie);
 
-  // to get rid with white space
-  const cookieData = document.cookie.split(';');
-  const data = cookieData.map(i => {
-    return i.trim();
-  });
-
-  // console.log(data);
-  console.log(data[1].split('=')[1]);
 });
