0e3548107f14c0a85564af007c28c8e83d2f617e
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-09 16:26:28 +0300

    Working with Cookies

--- Section 19 - Utilizing Browser Storage/app.js
+++ Section 19 - Utilizing Browser Storage/app.js
@@ -3,9 +3,16 @@ const retrBtn = document.getElementById('retrieve-btn');
 
 storBtn.addEventListener('click', () => {
   const userId = 'u123';
-  document.cookie =  `uid = ${userId}`;
+  const user = {name: 'Mustafa', age: 20};
+
+  document.cookie =  `uid = ${userId}; max-age=360`;
+  document.cookie =  `user = ${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener('click', () => {
-  console.log(document.cookie);
+  const cookieData = document.cookie.split(';');
+  const data = cookieData.map(i => {
+    return i.trim();
+  });
+  console.log(data[1].split('=')[1]); // user value
 });
