258ab81ea16048a39c400dc08ca2ed459f9e472e
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-10 13:16:37 +0300

    Feature Detection + Fallback Code (completed)

--- Section 20 - JavaScript & Browser Support/src/app.js
+++ Section 20 - JavaScript & Browser Support/src/app.js
@@ -1,5 +1,19 @@
 const button = document.querySelector('button');
+const textParagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
-  // do something...
-});
+  const text = textParagraph.textContent;
+  if (navigator.clipboard) {
+    navigator.clipboard
+    .writeText(text)
+    .then(result => {
+      console.log('Copied!');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  } else {
+    alert('Feature does\'nt work with your browser version.')
+  }
+
+}); 
