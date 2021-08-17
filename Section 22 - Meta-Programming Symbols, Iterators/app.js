327975de7becba3f6b073433fecf29092ad524a2
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-17 11:13:13 +0300

    Understanding Symbols

---
+++ Section 22 - Meta-Programming Symbols, Iterators/app.js
@@ -0,0 +1,21 @@
+// Library land
+const uid = Symbol();  // identifier: only for logging purposes
+console.log(uid);
+
+const user = {
+  // id: 'p1',
+  [uid]: 'p1',  // this make the value not touchable!
+  name: 'Mustafa',
+  age: 20
+};
+
+user[uid] = 'p3'; // the change only inside of the liabrary, not possible outside
+
+// App land => Using the library
+
+user.id = 'p2'; // this should not be possible!
+
+console.log(user);
+console.log(user[Symbol('uid')]);  // undefined
+console.log(Symbol('uid') === Symbol('uid'));
+console.log(user);
