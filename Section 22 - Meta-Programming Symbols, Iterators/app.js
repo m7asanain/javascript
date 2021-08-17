f9ae6ed64d94ff1818038fba7f94be52b6758614
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-17 12:01:11 +0300

    Understanding Iterators

--- Section 22 - Meta-Programming Symbols, Iterators/app.js
+++ Section 22 - Meta-Programming Symbols, Iterators/app.js
@@ -19,3 +19,30 @@ user.id = 'p2'; // this should not be possible!
 console.log(user[Symbol('uid')]);  // undefined
 console.log(Symbol('uid') === Symbol('uid'));
 console.log(user.toString()); // .toString() => will give: [object Object]*
+
+const company = {
+  curEmployee: 0,
+  employees: ['Mustafa', 'Abbas', 'Abdullah'],
+  next() {
+    if (this.curEmployee >= this.employees.length) {
+      return { value: this.curEmployee, done: true };
+    }
+    const returnValue = { value: this.employees[this.curEmployee], done: false }; // [this.curEmployee]: return list of names
+    this.curEmployee++;
+    return returnValue;
+  }
+};
+
+// console.log(company.next());  // {value: "Mustafa", done: false} 
+// console.log(company.next());  // {value: "Abbas", done: false}
+// console.log(company.next());  // {value: "Abdullah", done: false}
+// console.log(company.next());  // {value: 3, done: true}
+// console.log(company.next());  // {value: 3, done: true}
+
+let employee = company.next();
+
+while (!employee.done) {
+  console.log(employee.value);
+  employee = company.next();
+}
+console.log('That\'s it!');
