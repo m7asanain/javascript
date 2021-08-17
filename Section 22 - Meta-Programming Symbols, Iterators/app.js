1b2aadfffaf668907027866d137b5d94c11f4ab0
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-17 11:23:29 +0300

    Well-known Symbols

--- Section 22 - Meta-Programming Symbols, Iterators/app.js
+++ Section 22 - Meta-Programming Symbols, Iterators/app.js
@@ -6,7 +6,8 @@ const user = {
   // id: 'p1',
   [uid]: 'p1',  // this make the value not touchable!
   name: 'Mustafa',
-  age: 20
+  age: 20,
+  [Symbol.toStringTag]: 'User'  // [Symbol.toStringTag] => will give: [object User]*
 };
 
 user[uid] = 'p3'; // the change only inside of the liabrary, not possible outside
@@ -15,7 +16,6 @@ user[uid] = 'p3'; // the change only inside of the liabrary, not possible outsid
 
 user.id = 'p2'; // this should not be possible!
 
-console.log(user);
 console.log(user[Symbol('uid')]);  // undefined
 console.log(Symbol('uid') === Symbol('uid'));
-console.log(user);
+console.log(user.toString()); // .toString() => will give: [object Object]*
