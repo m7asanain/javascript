3e8819542e3e01e58608a3f22bda33c6031b192c
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-20 00:34:27 +0300

    Understanding Modules & File Access

--- Section 23 - Node.js/app.js
+++ Section 23 - Node.js/app.js
@@ -1,2 +1,17 @@
-const userName = 'Mustafa';
-console.log(`Hi ${userName}!`);
+const fs = require('fs');
+
+fs.readFile('user-data.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.toString());
+});
+
+fs.writeFile('user-data.txt', 'username=Mustafa', err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Wrote to file..');
+  }
+})
