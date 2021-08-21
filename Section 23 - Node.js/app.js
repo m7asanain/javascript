e95a01edf8c9447d2d2beb03f5af0cc38ad98f81
Mustafa Alkamali <m7asanain@gmail.com>
2021-08-21 19:09:43 +0300

    Rendering Server-side HTML with Templates & EJS

--- Section 23 - Node.js/app.js
+++ Section 23 - Node.js/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');  // parsing the request body and adding it to the request object
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');  // tell expressjs to use ejs (the engine for parsing our view should be ejs)
+app.set('views', 'views');  // when it find our views -> add a views setting and point at the folder that holds our views files
+
+app.use(bodyParser.urlencoded({extended: false}));  // adding it as a middleware
 
 app.use((req, res, next) => {
   res.setHeader('Content-Type', 'text/html');
@@ -11,8 +14,10 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  const userName = req.body.username || 'Unknown User';
-  res.send(`<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`);
+  const userName = req.body.userName || 'Unknown User!';
+  res.render('index', {
+    user: userName
+  });
 });
 
 app.listen(3000);
